8324fbc38b696f6ea89633c77a5536c0db507feb
BenjaminNT12 <benjaminnt12@gmail.com>
2022-12-07 00:51:34 -0600

    object detection

---
+++ objectDetectionTF.py
@@ -0,0 +1,30 @@
+import os
+import pathlib
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+import io
+import scipy.misc
+import numpy as np
+from six import BytesIO
+from PIL import Image, ImageDraw, ImageFont
+from six.moves.urllib.request import urlopen
+
+import tensorflow as tf
+import tensorflow_hub as hub
+
+tf.get_logger().setLevel('ERROR')
+
+
+
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.utils import ops as utils_ops
+
+# matplotlib inline
+
+
+PATH_TO_LABELS = './models/research/object_detection/data/mscoco_label_map.pbtxt'
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
