1968036e07e00868217405937bec99399c40e459
Benjamin Nicolas Trinidad <benjaminnt12@gmail.com>
2023-06-20 12:01:03 -0600

    Object detection con yolo v8

--- Chapter 6 - Yolo wit webcam/ObjectDetectionYOLO2.py
+++ Chapter 6 - Yolo wit webcam/ObjectDetectionYOLO2.py
@@ -3,17 +3,29 @@ import cv2 as cv
 import cvzone
 import math
 
-cap = cv.VideoCapture(0)
+# cap = cv.VideoCapture(0)
+# cap.set(3, 640)
+# cap.set(4, 480)
 
-cap.set(3, 640)
-cap.set(4, 480)
+cap = cv.VideoCapture("../Algoritmos_vision/Videos/motorbikes.mp4")
 
-model = YOLO('../Yolo-Weigths/yolov8n.pt')
+
+
+model = YOLO('../Yolo-Weigths/yolov8l.pt')
+
+className = ["Persona", "bicicleta", "automovil", "motocicleta", "casa", "plátano", "pelota", "teléfono", "ordenador", "semaforo", "libro", 
+                 "cuchara", "árbol", "sol", "lámpara", "camisa", "zapato", "sombrero", "taza", "piano", "mesa", "pantalla", "ratón", 
+                 "manzana", "naranja", "reloj", "papel", "lápiz", "puerta", "ventana", "gafas", "bicicleta", "jardín", "cartas", 
+                 "televisión", "maleta", "cama", "cámara", "aire acondicionado", "balón", "vaso", "reloj despertador", "flores", 
+                 "silla de ruedas", "candado", "auriculares", "radio", "pintura", "ropa", "bolígrafo", "peine", "escritorio", 
+                 "teclado", "percha", "laptop", "lente", "mochila", "abrelatas", "bombilla", "caja", "botella", "tetera", "cuchillo", 
+                 "plato", "sartén", "tenedor", "tijeras", "microondas", "basura", "aspiradora", "secadora", "lavadora", "espejo", 
+                 "pizarra", "horno", "cafetera", "frigorífico", "impresora", "altavoz"]
 
 
 while True:
     ret, frame = cap.read()
-    frame = cv.flip(frame, 1)
+    # frame = cv.flip(frame, 1)
     results = model(frame, stream=True)
     for r in results:
         boxes = r.boxes
@@ -27,8 +39,11 @@ while True:
 
             conf = math.ceil(box.conf[0]*100)/100
             print(conf)
-            cvzone.putTextRect(frame, f'{conf}', (x1, y1-10), 2, 2, offset=10, border=5)
+            
 
+            cls = int(box.cls[0])
+
+            cvzone.putTextRect(frame, f'{className[cls]}  {conf}', (max(0, x1), max(35,y1)), scale=0.7, thickness=1, colorB=(0,255,0))
 
     cv.imshow('frame', frame)
     cv.waitKey(1)
