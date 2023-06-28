212a6832dbe851706f8e9eb844ae752b3055f9ab
Benjamin Nicolas Trinidad <benjaminnt12@gmail.com>
2023-06-28 16:58:35 -0600

    evaluacion de algoritmos de tracking

--- distancia_MOSSE.py
+++ distancia_MOSSE.py
@@ -3,13 +3,13 @@ import numpy as np
 import math
 import  matplotlib.pyplot as plt
 
-# path = '/home/nicolas/Github/Algoritmos_vision/video3.mp4'
-path = 0
+path = '/home/nicolas/Github/videos/video9.MOV'
+# path = 0
 
 tracker = cv.legacy.TrackerMOSSE_create()
 
-start_second = 80
-restart_second = 80
+start_second = 18
+restart_second = 18
 
 ix, iy = 0, 0
 xf, yf = 0, 0
@@ -76,7 +76,7 @@ while True:
     if ret == False: break
 
     frame = cv.flip(frame, 1)
-    frame_enhaced = mejorar_imagen(frame)
+    # frame = mejorar_imagen(frame)
 
     if cv.waitKey(1) & 0xFF == ord(' '):
         while True:
@@ -85,14 +85,14 @@ while True:
             if selection == False:
                 cv.namedWindow('frame')
                 cv.setMouseCallback('frame', draw_rectangle)
-                cv.addWeighted(frame_enhaced, 1, rectangle_mask, 1, 0, temp_frame)
+                cv.addWeighted(frame, 1, rectangle_mask, 1, 0, temp_frame)
 
             if selection == True and init_tracking == False:
                 start_frame = get_frame_number(cap, restart_second)
                 init_tracking = True
                 cap.set(cv.CAP_PROP_POS_FRAMES, start_frame)
                 bbox = np.array([ix, iy, xf - ix, yf - iy])
-                tracker.init(frame_enhaced,bbox)
+                tracker.init(frame,bbox)
 
             cv.imshow('frame', temp_frame)
 
@@ -100,11 +100,11 @@ while True:
                 break
 
     if init_tracking == True:
-        success, bbox = tracker.update(frame_enhaced)
+        success, bbox = tracker.update(frame)
         x, y, w, h = [int(coord) for coord in bbox]
-        cv.rectangle(frame_enhaced, (x, y), (x + w, y + h), (0, 0, 255), 1)   
+        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 1)   
 
-    cv.imshow('frame', frame_enhaced)
+    cv.imshow('frame', frame)
 
     if cv.waitKey(1) & 0xFF == 0x1B:
         break
