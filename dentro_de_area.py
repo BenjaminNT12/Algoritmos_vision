d5990f7587c77d39f2a917aca8ea30857f88763a
Benjamin Nicolas Trinidad <benjaminnt12@gmail.com>
2023-07-04 18:43:41 -0600

    actualizacion en algoritmo de distancia color

--- dentro_de_area.py
+++ dentro_de_area.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math 
 
 def dentro_de_area(coordenadas, punto_referencia, radio):
     # Calcula la distancia euclidiana entre cada coordenada y el punto de referencia
@@ -9,14 +10,79 @@ def dentro_de_area(coordenadas, punto_referencia, radio):
 
     return coordenadas_dentro_area
 
-# Ejemplo de uso
-coordenadas = np.array([[410, 381], [162, 375], [287, 191], [424, 128], [148, 122]])
-punto_referencia = np.array([165, 355])
-radio = 25
+def dentro_de_area2(coordenadas, punto_referencia, radio):
 
-coordenadas[0] = dentro_de_area(coordenadas, coordenadas[1], 25) 
+    # Calcula la distancia euclidiana entre cada coordenada y el punto de referencia
+    distancias = np.linalg.norm(coordenadas - punto_referencia, axis=1)
 
-print("coordenadas", coordenadas,"test", coordenadas[0])
+    # Verifica si la distancia es menor que el radio
+    coordenadas_dentro_area = coordenadas[distancias < radio]
 
-coordenadas_dentro = dentro_de_area(coordenadas, punto_referencia, radio)
-print(coordenadas_dentro)
+    # Obtiene la posición de las coordenadas dentro del arreglo
+    posiciones_dentro_area = np.where(distancias < radio)[0]
+
+    return coordenadas_dentro_area, posiciones_dentro_area
+
+def distance(point1, point2):
+  """Calculates the distance between two points.
+
+  Args:
+    point1: A tuple of (x, y) coordinates.
+    point2: A tuple of (x, y) coordinates.
+
+  Returns:
+    The distance between the two points.
+  """
+
+  x_diff = point1[0] - point2[0]
+  y_diff = point1[1] - point2[1]
+  return math.sqrt(x_diff**2 + y_diff**2)
+
+def find_closest_point(array, coordinates):
+  """Finds the index of the point in the array that is closest to the given coordinates.
+
+  Args:
+    array: A list of points, each represented as a tuple of (x, y) coordinates.
+    coordinates: The coordinates of the point to find the closest point to.
+
+  Returns:
+    The index of the point in the array that is closest to the given coordinates.
+  """
+
+  # Calculate the distance between each point in the array and the given coordinates.
+  distances = []
+  for i, point in enumerate(array):
+    distances.append(distance(point, coordinates))
+
+  # Find the index of the point with the smallest distance.
+  closest_index = distances.index(min(distances))
+
+  return closest_index
+
+
+coordenadas = np.array([[388, 429], [72, 424], [243, 192], [398, 130], [81, 105]])
+
+punto = np.array([253, 195])
+
+print("coordenadas", coordenadas)
+
+coordenadas_nuevas, posicion = dentro_de_area2(coordenadas, punto, 50)
+np.put(coordenadas, [len(punto)*posicion, len(punto)*posicion+1], punto)
+print("posicion", posicion, "punto", punto[:])
+print("coordenadas", coordenadas, "posicion", posicion)
+
+
+
+coordenadas = np.array([[243, 192], [388, 429], [72, 424], [398, 130], [81, 105]])
+
+punto = np.array([253, 195])
+
+print("coordenadas", coordenadas)
+
+coordenadas_nuevas, posicion = dentro_de_area2(coordenadas, punto, 50)
+np.put(coordenadas, [len(punto)*posicion, len(punto)*posicion+1], punto)
+print("posicion", posicion, "punto", punto[:])
+print("coordenadas", coordenadas, "posicion", posicion)
+
+pos = find_closest_point(punto, coordenadas)
+print("pos", pos)
--- distancia.py
+++ distancia.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-import time
+import math
 
 # path = '/home/nicolas/Github/Algoritmos_vision/video3.mp4'
 # lower_color [135   2 254] upper_color [158   4 255]
@@ -8,9 +8,10 @@ path = 0
 
 name = False
 count = [0]
+set_rotation = 0.0
 
 coordenadas = []
-
+new_cordinates = np.array([])
 start_second = 80
 restart_second = 80
 
@@ -60,30 +61,86 @@ def split_hsv_channels(frame):
     return h, s, v
 
 def dentro_de_area(coordenadas, punto_referencia, radio):
+
     # Calcula la distancia euclidiana entre cada coordenada y el punto de referencia
     distancias = np.linalg.norm(coordenadas - punto_referencia, axis=1)
 
     # Verifica si la distancia es menor que el radio
     coordenadas_dentro_area = coordenadas[distancias < radio]
 
-    return coordenadas_dentro_area
+    # Obtiene la posición de las coordenadas dentro del arreglo
+    posiciones_dentro_area = np.where(distancias < radio)[0]
+
+    return posiciones_dentro_area
+
+def calcular_pose(puntos):
+    # Verificar que haya exactamente 5 puntos
+    if puntos.shape != (5, 2):
+        raise ValueError("Se requiere una matriz de 5 filas y 2 columnas para calcular la pose.")
+
+    # Extraer las coordenadas x e y de los puntos
+    x = puntos[:, 0]
+    y = puntos[:, 1]
+
+    # Calcular el centro de masa de los puntos
+    centro_x = np.mean(x)
+    centro_y = np.mean(y)
+
+    # Calcular las coordenadas relativas a partir del centro de masa
+    x_rel = x - centro_x
+    y_rel = y - centro_y
+
+    # Construir la matriz de covarianza
+    matriz_cov = np.cov(np.array([x_rel, y_rel]))
+
+    # Calcular los vectores y valores propios de la matriz de covarianza
+    valores_propios, vectores_propios = np.linalg.eig(matriz_cov)
+
+    # Encontrar el índice del valor propio más pequeño
+    indice_menor_valor = np.argmin(valores_propios)
+
+    # Obtener el vector propio correspondiente al menor valor propio
+    vector_menor_valor = vectores_propios[:, indice_menor_valor]
+
+    # Calcular la orientación relativa de la pose en grados
+    orientacion_rad = np.arctan2(vector_menor_valor[1], vector_menor_valor[0])
+    orientacion_deg = np.degrees(orientacion_rad)
+
+    # Calcular la posición relativa de la pose
+    posicion = np.array([centro_x, centro_y])
+
+    return posicion, orientacion_deg
+
+# crea una funcion que grafique una linea sobre un frame de opencv, y que argumento le de un angulo y lo grafique?
+def draw_line(img, start_point, end_point, color = (0,0,255), thickness = 1):
+  # Convert the angle to radians.
+#   angle = angle * math.pi / 180
+
+  # Calculate the start and end points of the line.
+  start_x = start_point[0] 
+  start_y = start_point[1] 
+  end_x = end_point[0]
+  end_y = end_point[1]
+#   end_x = start_x + math.cos(angle) * img.shape[1]
+#   end_y = start_y + math.sin(angle) * img.shape[0]
+
+  # Draw the line.
+  cv.line(img, (int(start_x), int(start_y)), (int(end_x), int(end_y)), color, thickness)
+
+
 
 def color_tracking(frame, lower_color, upper_color):
-    global name
-    id = 0
+    global name, coordenadas, new_cordinates, set_rotation
+    position = 0
+    translacion = 0
+
     
     hsv_frame = cv.cvtColor(frame, cv.COLOR_RGB2HSV)
-    # cv.imshow('hsv_frame', hsv_frame)
     color_mask = cv.inRange(hsv_frame, lower_color, upper_color)
-    # cv.imshow('color_mask', color_mask)
 
     color_mask = cv.erode(color_mask, None, iterations = 2)
     color_mask = cv.dilate(color_mask, None, iterations = 2)
-
-    # print("lower_color",lower_color, "upper_color", upper_color)
-    # imprimir la dimension de la mascara
-    # print(color_mask.shape)
-
+    
     # Encontrar los contornos de los objetos de color
     contours, _ = cv.findContours(color_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
@@ -99,23 +156,36 @@ def color_tracking(frame, lower_color, upper_color):
             centroid_x = int(M["m10"] / M["m00"])
             centroid_y = int(M["m01"] / M["m00"])
             
-            
             if name == False:
-                coordenadas.append([centroid_x, centroid_y])
+                coordenadas.append([centroid_x, centroid_y])   
+                new_cordinates = np.array(coordenadas) 
             else:
-                coordenadas[id] = dentro_de_area(coordenadas, coordenadas[id], 25)  
-                print("coordenadas[id]: ",coordenadas[id])  
-            id += 1    
-            
-                # coordenadas[id] = [centroid_x, centroid_y]
-            
-            cv.putText(frame, str(id), (centroid_x - 25, centroid_y - 25), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                actual_coordinates = np.array([centroid_x, centroid_y])
+                position = dentro_de_area(new_cordinates, actual_coordinates, 50)
+                
+                np.put(new_cordinates, [len(actual_coordinates)*position, len(actual_coordinates)*position+1], actual_coordinates)
+                translacion, rotacion = calcular_pose(new_cordinates)
+
+                cv.putText(frame, str(translacion), (20, 50), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                
+                cv.circle(frame, (int(translacion[0]), int(translacion[1])), 5, (0, 0, 255), -1)
+                draw_line(frame, translacion, new_cordinates[4][:])
+                x = new_cordinates[4][1] - translacion[1]
+                y = new_cordinates[4][0] - translacion[0]
+
+                angle = math.atan2(y, x) * (180.0 / math.pi)
+                if set_rotation == 0.0:
+                    set_rotation = angle
+                    print("set_rotation", set_rotation)
+                angle = angle - set_rotation
+                # print('Ángulo en grados: ' + str(angle))
+                cv.putText(frame, str(angle), (20, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                
+            cv.putText(frame, str(position), (centroid_x - 25, centroid_y - 25), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
             cv.circle(frame, (centroid_x, centroid_y), 12, (0, 255, 0), -1)
-    print("coordenadas", coordenadas,"coordenadas 1", coordenadas[1])
+            
     name = True
-
     return color_mask
-
 
 def get_frame_number(video, second):
     fps = video.get(cv.CAP_PROP_FPS)
@@ -160,8 +230,11 @@ def mejorar_imagen(frame):
 
 start_frame = get_frame_number(cap, start_second)
 cap.set(cv.CAP_PROP_POS_FRAMES, start_frame)
-
-
+  
+  
+fourcc = cv.VideoWriter_fourcc(*'XVID')
+out = cv.VideoWriter("orientacion.avi", fourcc, 30, ((int(cap.get(4)), int(cap.get(3)))))
+  
 while True:
     
     ret, frame = cap.read()
@@ -170,38 +243,118 @@ while True:
     frame = cv.flip(frame, 1)
     # frame = mejorar_imagen(frame)
 
-    # if cv.waitKey(1) & 0xFF == ord(' '):
-    #     while True:
-    #         temp_frame = np.zeros((int(cap.get(4)), int(cap.get(3)), 3), dtype=np.uint8)
-            
-    #         if rectangle_complete == False:
-    #             cv.namedWindow('frame')
-    #             cv.setMouseCallback('frame', draw_rectangle)
-    #             cv.addWeighted(frame, 1, rectangle_mask, 1, 0, temp_frame)
-
-    #         if rectangle_complete == True and hsv_range_complete == False:
-    #             get_hsv_range(frame, ix, iy, xf, yf)
-    #             hsv_range_complete = True
-    #             start_frame = get_frame_number(cap, restart_second)
-    #             cap.set(cv.CAP_PROP_POS_FRAMES, start_frame)
-
-    #         cv.imshow('frame', temp_frame)
-
-    #         if cv.waitKey(1) & 0xFF == ord(' ') or hsv_range_complete == True:
-    #             break
-
     if hsv_range_complete == True or cv.waitKey(1) & 0xFF == ord('a'):
         hsv_range_complete = True
         mask = color_tracking(frame, lower_color, upper_color)
-        # cv.imshow('mask', mask)
     
     cv.imshow('frame', frame)
-    
-    # time.sleep(0.02)
-
+    out.write(frame)
     if cv.waitKey(1) & 0xFF == 0x1B:
         break
 cap.release()
+out.release()
 cv.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# while True:
+    
+#     ret, frame = cap.read()
+#     if ret == False: break
+
+#     frame = cv.flip(frame, 1)
+#     # frame = mejorar_imagen(frame)
+
+#     # if cv.waitKey(1) & 0xFF == ord(' '):
+#     #     while True:
+#     #         temp_frame = np.zeros((int(cap.get(4)), int(cap.get(3)), 3), dtype=np.uint8)
+            
+#     #         if rectangle_complete == False:
+#     #             cv.namedWindow('frame')
+#     #             cv.setMouseCallback('frame', draw_rectangle)
+#     #             cv.addWeighted(frame, 1, rectangle_mask, 1, 0, temp_frame)
+
+#     #         if rectangle_complete == True and hsv_range_complete == False:
+#     #             get_hsv_range(frame, ix, iy, xf, yf)
+#     #             hsv_range_complete = True
+#     #             start_frame = get_frame_number(cap, restart_second)
+#     #             cap.set(cv.CAP_PROP_POS_FRAMES, start_frame)
+
+#     #         cv.imshow('frame', temp_frame)
+
+#     #         if cv.waitKey(1) & 0xFF == ord(' ') or hsv_range_complete == True:
+#     #             break
+
+#     if hsv_range_complete == True or cv.waitKey(1) & 0xFF == ord('a'):
+#         hsv_range_complete = True
+#         mask = color_tracking(frame, lower_color, upper_color)
+#         # cv.imshow('mask', mask)
+    
+#     cv.imshow('frame', frame)
+    
+#     # time.sleep(0.02)
+
+#     if cv.waitKey(1) & 0xFF == 0x1B:
+#         break
+# cap.release()
+# cv.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
