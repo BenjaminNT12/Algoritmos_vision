cfb965f42599a42e72c62488d3b7a74440a4e966
Benjamin Nicolas <benjaminnt12@gmail.com>
2023-08-21 18:46:46 -0600

    algo de estudio

---
+++ algoritmos/estudios.py
@@ -0,0 +1,19 @@
+# def main():
+#     x = input( "Enter a name: " )
+    
+#     print("Hola " + x)
+    
+# if __name__ == "__main__":
+#     main()
+
+from iteraTools import combinations
+
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+
+    return 0
+
+if __name__ == '__main__':
+    main()
