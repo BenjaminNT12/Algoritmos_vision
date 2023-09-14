d5c6610b017673cd2bffde6ae1a31b82db9bd733
Benjamin Nicolas <benjaminnt12@gmail.com>
2023-09-14 16:57:51 -0600

    peter paul inequality

--- peterPaulInequality.py
+++ peterPaulInequality.py
@@ -1,15 +1,17 @@
 import numpy as np
 
-def peter_paul_inequality_minus(a, b, c):
+def peter_paul_inequality_minus(a, b):
   """Returns True if a - b >= c, False otherwise."""
-  return a - b >= c
+  print(f"AB = {-a*b}")
+  print(f"(a^2)/2 + (b^2)/2 = {((a**2)/2 + (b**2)/2)}" )
+  return -a*b <= ((a**2)/2 + (b**2)/2)
 
 # Check if a - b >= c
-a = 10
-b = 5
-c = 3
+a = -1
+b = -1
 
-if peter_paul_inequality_minus(a, b, c):
-  print("a - b >= c")
+
+if peter_paul_inequality_minus(a, b):
+  print("a*b <= (a - b)")
 else:
-  print("a - b < c")
+  print("No se cumple")
